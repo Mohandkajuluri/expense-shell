983c00abe148f7975e4e072bca87b5e2a1a81533
Mohandkajuluri <md.kajuluri@gmail.com>
2023-11-02 20:28:41 -0400

    code edited

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
 dnf install nginx -y
+echo $?
 
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
 
 rm -rf /usr/share/nginx/html/*
+echo $?
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo $?
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo $?
 
 systemctl enable nginx
 systemctl start nginx
-
+echo $?
